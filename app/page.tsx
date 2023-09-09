1c5781224e183ea9c70dde5719ae1a53edd5713f
dillonzellis <dillonzellis@gmail.com>
2023-09-09 10:54:40 -0400

    add logged in logic to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,9 +7,21 @@ export default function Home() {
     <main className="h-screen w-full bg-slate-950 text-white flex flex-col justify-center items-center">
       <div className="space-y-4">
         <div className="italic text-5xl font-bold">Family Pic</div>
-        <Button asChild className="bg-red-900 hover:bg-red-500/80">
-          <Link href="/">Log In</Link>
-        </Button>
+        <div className="space-x-4">
+          <SignedIn>
+            <Button asChild className="bg-red-900 hover:bg-red-500/80">
+              <Link href="/dashboard">Dashboard</Link>
+            </Button>
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="bg-red-900 hover:bg-red-500/80">
+              <Link href="/sign-in">Log In</Link>
+            </Button>
+            <Button asChild className="bg-red-900 hover:bg-red-500/80">
+              <Link href="/sign-up">Sign Up</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </main>
   );
